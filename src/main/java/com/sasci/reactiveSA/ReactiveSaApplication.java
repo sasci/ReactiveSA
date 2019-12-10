e714ddaa21aeeddc2295ebd04d6f77e14196e666
sasci <tunca0173@gmail.com>
2019-12-10 10:47:30 +0000

    Spring project created

---
+++ src/main/java/com/sasci/reactiveSA/ReactiveSaApplication.java
@@ -0,0 +1,13 @@
+package com.sasci.reactiveSA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveSaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sasci/reactiveSA/ReactiveSaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sasci.reactiveSA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveSaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
